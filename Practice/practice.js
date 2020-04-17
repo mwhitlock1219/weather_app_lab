723bf2a0d4df8ebda6ca42b1f108a320da6e8c24
Melina Alejos <malejos1219@gmail.com>
2020-04-17 16:22:15 -0400

    third commit

---
+++ Practice/practice.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$('#getWeatherForcast').click(function(){
+		var zipcode = $('#zip').val();
+		var key = 'c65692185192c2852cfc0a2ec9095d2a'
+
+		$.ajax({
+			url:'https://api.openweathermap.org/data/2.5/weather',
+			dataType:'json',
+			type:'GET',
+			data:{zip:zipcode, appid:key, units:'imperial'},
+
+			success: function(data){
+				var wf = '';
+				$.each(data.weather,function(index, val){
+					wf += '<p><b>'+data.name+'</b></p>'+data.main.temp+ '&deg;F '+' | '+ val.main+", "+val.description
+				});
+				$('#showWeatherForcast').html(wf);
+			}
+		})
+	})
+})
+
--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-$(document).ready(function() {
-  var weatherApiUrl="https://api.openweathermap.org/data/2.5/weather";
-  var apiKey = "c65692185192c2852cfc0a2ec9095d2a"; //<weather-api-keys>
-  getLocation(); 
-});
+$(document).ready(function(){
+  $('#getWeatherForcast').click(function(){
+    var zipcode = $('#zip').val();
+    var key = 'c65692185192c2852cfc0a2ec9095d2a'
 
-function getLocation(){
+    $.ajax({
+      url:'https://api.openweathermap.org/data/2.5/weather',
+      dataType:'json',
+      type:'GET',
+      data:{zip:zipcode, appid:key, units:'imperial'},
 
-}
-
-(function()
-{
-  // instantiate a moment object
-  var NowMoment = moment();
+      success: function(data){
+        var wf = '';
+        $.each(data.weather,function(index, val){
+          wf += '<p><b>'+data.name+'</b></p>'+data.main.temp+ '&deg;F '+' | '+ val.main+", "+val.description
+        });
+        $('#showWeatherForcast').html(wf);
+      }
+    })
+  })
+  (function(){
   
-  // instantiate a JavaScript Date object
-  var NowDate = new Date();
+  var NowMoment = moment(); // instantiate a moment object
+  
+  
+  var NowDate = new Date();  // instantiate a JS Date object
   
   // display value of moment object in #displayMoment div
   var eDisplayMoment = document.getElementById('displayMoment');
@@ -24,3 +33,23 @@ function getLocation(){
   var eDisplayDate = document.getElementById('displayJsDate');
   eDisplayDate.innerHTML = NowDate;
 })();
+})
+
+
+
+
+// (function(){
+  
+//   var NowMoment = moment(); // instantiate a moment object
+  
+  
+//   var NowDate = new Date();  // instantiate a JS Date object
+  
+//   // display value of moment object in #displayMoment div
+//   var eDisplayMoment = document.getElementById('displayMoment');
+//   eDisplayMoment.innerHTML = NowMoment;
+  
+//   // display value of Date object in #displayJsDate div
+//   var eDisplayDate = document.getElementById('displayJsDate');
+//   eDisplayDate.innerHTML = NowDate;
+// })();
