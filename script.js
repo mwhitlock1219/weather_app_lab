3d4ac5bae6f402671c4a547c29fc9cf5d8d0075a
Melina Alejos <malejos1219@gmail.com>
2020-04-17 11:25:52 -0400

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-// $(document).ready(function() {
-//   var fahrenheit, celsius;
-//   var weatherApiUrl="https://api.openweathermap.org/data/2.5/weather";
-//   var apiKey = "c65692185192c2852cfc0a2ec9095d2a"; //<weather-api-keys>
-//   getLocation();  /*getLatLong(); changed*/
-// });
-console.log('this works');
+$(document).ready(function() {
+  var weatherApiUrl="https://api.openweathermap.org/data/2.5/weather";
+  var apiKey = "c65692185192c2852cfc0a2ec9095d2a"; //<weather-api-keys>
+  getLocation(); 
+});
 
-// var moment = require('moment');
+function getLocation(){
 
-// moment().format();
+}
+
+(function()
+{
+  // instantiate a moment object
+  var NowMoment = moment();
+  
+  // instantiate a JavaScript Date object
+  var NowDate = new Date();
+  
+  // display value of moment object in #displayMoment div
+  var eDisplayMoment = document.getElementById('displayMoment');
+  eDisplayMoment.innerHTML = NowMoment;
+  
+  // display value of Date object in #displayJsDate div
+  var eDisplayDate = document.getElementById('displayJsDate');
+  eDisplayDate.innerHTML = NowDate;
+})();
