391ad1004e648e0f0104ab582be21c1301013d06
Melina Alejos <malejos1219@gmail.com>
2020-04-17 10:15:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//   var fahrenheit, celsius;
+//   var weatherApiUrl="https://api.openweathermap.org/data/2.5/weather";
+//   var apiKey = "c65692185192c2852cfc0a2ec9095d2a"; //<weather-api-keys>
+//   getLocation();  /*getLatLong(); changed*/
+// });
+console.log('this works');
+
+// var moment = require('moment');
+
+// moment().format();
